ef102ca4cb107535d575d42e30516b8e45c6396a
Lucas Marques Santos <lucasmarks1991@hotmail.com>
2025-06-02 17:29:44 -0300

    fix(build): correção para não fazer cache de views no entrypoint

--- docker/prod/entrypoint.sh
+++ docker/prod/entrypoint.sh
@@ -10,9 +10,6 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Caching views..."
-php artisan view:cache
-
 echo "Clearing application cache..."
 php artisan cache:clear
 
